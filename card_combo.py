86919c1d4911fa80eebb53bdd2eeafbf3717858d
kellei <kelvin.lei3@gmail.com>
2018-10-24 21:03:54 -0700

    modified card combo

--- card_combo.py
+++ card_combo.py
@@ -1,8 +1,20 @@
+'''
+a is a list of avialable cards
+b is the goal value
+c is a blank list
+
+for every recursive loop, c stores 1 value from list a if the sume is below
+the goal value b
+
+how to add up all the match-upi scenarios between the loops
+'''
+
 def card_combo (a,b,c=[]):
 	total = sum(c)
-	combo_num = 0
 
 	if total == b:
+		#this combo adds up to goal value
+		print ("{}".format(c))
 		return 1
 
 	elif total > b:
@@ -11,10 +23,9 @@ def card_combo (a,b,c=[]):
 
 	else:
 		for i in range(len(a)):
-			card_combo(a[i+1:], b, c+[a[i]])
+			return card_combo(a[i+1:], b, c+[a[i]])
 
-	print(combo_num)
+cards = [1,1,1,2]
 
-cards = [1,1,2]
+print(card_combo(cards,3))
 
-card_combo(cards,3)
